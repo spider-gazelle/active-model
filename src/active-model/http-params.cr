86fd1d726801430a28e3e62a50a85bb09f260716
Caspian Baska <email@caspian.computer>
2021-06-24 12:12:26 +1000

    ci: remove redundant flag

--- src/active-model/http-params.cr
+++ src/active-model/http-params.cr
@@ -1,8 +1,10 @@
+# :nodoc:
 struct Time
+  # :nodoc:
   # Parse `Time` from an HTTP param as unix timestamp.
   def self.from_http_param(value : String) : Time
     Time.unix(value.to_i64)
   rescue ArgumentError
-    raise TypeCastError.new
+    raise TypeCastError.new("Failed to cast #{value} to unix epoch")
   end
 end
